8c1a05490c425b9e0fa0b48e8ba92d2799101528
Håvard Christensen <havchr@agens.no>
2023-07-17 13:18:38 +0200

    Now we are rendering with textures

--- src/lib.rs
+++ src/lib.rs
@@ -166,6 +166,7 @@ struct State {
     window: Window,
     num_indices: u32,
     texture_rtt:wgpu::Texture,
+    diffuse_bind_group:wgpu::BindGroup,
 
 }
 
@@ -188,6 +189,16 @@ impl State {
         // it , State owns the window , sol this should be safe?.. I guess.
         let surface = unsafe{ instance.create_surface(&window) }.unwrap();
 
+
+        //load image
+        let diffuse_bytes = include_bytes!("diffuse.png");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.to_rgba8();
+
+        use image::GenericImageView;
+        let dimensions = diffuse_image.dimensions();
+
+
         let adapter = instance.request_adapter(
             &wgpu::RequestAdapterOptions {
                 power_preference: wgpu::PowerPreference::HighPerformance,
@@ -209,6 +220,99 @@ impl State {
             },
             None,
         ).await.unwrap();
+
+
+        let diffuse_texture_size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth_or_array_layers: 1,
+        };
+
+        let diffuse_texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                size: diffuse_texture_size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                label: Some("Diffuse_texture"),
+                view_formats: &[],
+            }
+        );
+
+        queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &diffuse_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &diffuse_rgba,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(4 * dimensions.0),
+                rows_per_image: Some(dimensions.1),
+            },
+            diffuse_texture_size,
+        );
+
+        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u : wgpu::AddressMode::ClampToEdge,
+            address_mode_v : wgpu::AddressMode::ClampToEdge,
+            address_mode_w : wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+
+
+        let texture_bind_group_layout = 
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float {
+                                filterable:true
+                            },
+                        },
+                        count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count: None,
+                },
+                ],
+                label: Some("texture_bind_group_layout"),
+            });
+
+        let diffuse_bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: &texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                    }
+                ],
+                label: Some("diffuse_bind_group"),
+            }
+        );
+
+
 
         let surface_caps = surface.get_capabilities(&adapter);
 
@@ -274,6 +378,7 @@ impl State {
                 label: Some("Render Pipeline Layout"),
                 bind_group_layouts: &[
                     &uniform_bind_group_layout,
+                    &texture_bind_group_layout,
                 ],
                 push_constant_ranges: &[],
             });
@@ -415,6 +520,7 @@ impl State {
             uniforms,
             window,
             texture_rtt,
+            diffuse_bind_group,
         }
     }
 
@@ -451,7 +557,7 @@ impl State {
         });
 
         //I am trying to render to texture but it says my format is just too wrong
-        ///*
+        /*
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("My First Render Pass to RTT"),
@@ -479,7 +585,7 @@ impl State {
             render_pass.draw_indexed(0..self.num_indices,0,0..1);
             //render_pass.draw(0..self.num_vertices,0..1);
         }
-        //*/
+        */
 
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
@@ -503,6 +609,7 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_bind_group(0,&self.uniform_bind_group,&[]);
+            render_pass.set_bind_group(1,&self.diffuse_bind_group,&[]);
             render_pass.set_vertex_buffer(0,self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..),wgpu::IndexFormat::Uint16);
             render_pass.draw_indexed(0..self.num_indices,0,0..1);
--- src/shaper_shader.wgsl
+++ src/shaper_shader.wgsl
@@ -9,6 +9,11 @@ iTime:f32,
 @group(0) @binding(0)
 var<uniform> uniforms: Uniforms;
 
+@group(1) @binding(0)
+var t_diffuse: texture_2d<f32>;
+@group(1) @binding(1)
+var s_diffuse: sampler;
+
 struct VertexInput {
 	@location(0) position: vec3<f32>,
 	@location(1) color: vec3<f32>,
@@ -28,7 +33,7 @@ fn vs_main(
 	var out: VertexOutput;
 	out.color = model.color;
 	out.clip_position = vec4<f32>(model.position, 1.0);
-	out.uv = vec2<f32>(model.position.x*0.5, model.position.y*0.5)+ vec2<f32>(0.5,0.5);
+	out.uv = vec2<f32>(model.position.x*0.5, model.position.y*0.5*-1.0)+ vec2<f32>(0.5,0.5);
 
 	return out;
 }
@@ -52,7 +57,8 @@ fn smoothedCircle(pos : vec2<f32> ,size:f32,smoothFactor:f32,uv:vec2<f32>) -> f3
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
 	let smoo = smoothedCircle(vec2<f32>(uniforms.iMouse.x + sin(uniforms.iTime*0.3)*0.66,uniforms.iMouse.y),0.2,0.1,in.uv) + 0.25;
-	return vec4<f32>(in.color*smoo,1.0);
+	let texSample : vec4<f32> = textureSample(t_diffuse,s_diffuse,in.uv);
+	return vec4<f32>(in.color*smoo,1.0) + texSample;
 }
 
 
