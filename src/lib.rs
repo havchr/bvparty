878795f658d8101e9ffa71a5b0a5597dd745a579
Håvard Christensen <havchr@agens.no>
2023-07-29 19:28:49 +0200

    created module nocmp and a texture submodule , this offloads some texture code and is my beginning of knowing how to divide up my rust code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod nocmp;
+
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -171,7 +173,7 @@ struct State {
 }
 
 impl State {
-    //Creating some ofthe wgpu types requires async code
+    //Creating some of the wgpu types requires async code
     async fn new(window: Window) -> Self {
         let size = window.inner_size();
 
@@ -193,6 +195,8 @@ impl State {
         //load image
         let diffuse_bytes = include_bytes!("diffuse.png");
         let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+
+
         let diffuse_rgba = diffuse_image.to_rgba8();
 
         use image::GenericImageView;
@@ -207,7 +211,7 @@ impl State {
             },
         ).await.unwrap();
 
-        //let us create the deviceand queue
+        //let us create the device and queue
         let(device,queue) = adapter.request_device(
             &wgpu::DeviceDescriptor {
                 features: wgpu::Features::empty(),
@@ -221,52 +225,6 @@ impl State {
             None,
         ).await.unwrap();
 
-
-        let diffuse_texture_size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            depth_or_array_layers: 1,
-        };
-
-        let diffuse_texture = device.create_texture(
-            &wgpu::TextureDescriptor {
-                size: diffuse_texture_size,
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8UnormSrgb,
-                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-                label: Some("Diffuse_texture"),
-                view_formats: &[],
-            }
-        );
-
-        queue.write_texture(
-            wgpu::ImageCopyTexture {
-                texture: &diffuse_texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            &diffuse_rgba,
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
-            },
-            diffuse_texture_size,
-        );
-
-        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u : wgpu::AddressMode::ClampToEdge,
-            address_mode_v : wgpu::AddressMode::ClampToEdge,
-            address_mode_w : wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
 
 
 
@@ -295,17 +253,18 @@ impl State {
                 label: Some("texture_bind_group_layout"),
             });
 
+        let diffyn = nocmp::texture::Texture::from_bytes(&device,&queue,include_bytes!("diffuse.png"),"testing").unwrap();
         let diffuse_bind_group = device.create_bind_group(
             &wgpu::BindGroupDescriptor {
                 layout: &texture_bind_group_layout,
                 entries: &[
                     wgpu::BindGroupEntry {
                         binding: 0,
-                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                        resource: wgpu::BindingResource::TextureView(&diffyn.view),
                     },
                     wgpu::BindGroupEntry {
                         binding: 1,
-                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                        resource: wgpu::BindingResource::Sampler(&diffyn.sampler),
                     }
                 ],
                 label: Some("diffuse_bind_group"),
@@ -586,6 +545,7 @@ impl State {
             //render_pass.draw(0..self.num_vertices,0..1);
         }
         */
+
 
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
